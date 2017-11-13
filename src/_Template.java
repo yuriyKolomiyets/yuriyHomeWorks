2f2a7a3956501b0cfcc50c99a092f1d42e0c1573
yuriykolomiyets <juriy121@gmail.com>
2017-11-13 21:20:52 +0200

    just a template of common syntax

---
+++ src/_Template.java
@@ -0,0 +1,165 @@
+import java.util.Arrays;
+
+
+public class _Template {
+
+
+
+/*
+
+int a;
+
+switch (a) {
+
+case 0:
+a++;
+break;
+case 1:
+a+1;
+break;
+
+default:
+a = a+5;
+
+}
+ */
+
+    /*
+   Объявляем масив:
+
+    int [] result = new int [2];
+
+    result [0] = nums[0];
+   result [1] = nums[nums.length-1];
+
+    return result;
+
+    или
+
+    int [] newarr = {nums[1], nums[2], nums[0]};
+
+     */
+
+    /*
+
+    return new int[] {3,1,4};
+
+   int[] array = new int[3];
+
+  array[0] = 3;
+  array[1] = 1;
+  array[2] = 4;
+
+  return array;
+
+
+     */
+
+/*
+
+импорт сканнера:
+
+import java.util.Scanner;
+
+
+ */
+
+/*
+
+  Scanner scanner = new Scanner(System.in);
+
+        int firstN = scanner.nextInt();
+ */
+
+/*
+
+метод к перенеменным:
+
+result = sum( firstN, secondN);
+
+метод:
+
+  static int sum(int a, int b) {
+int sum = a + b;
+
+return sum;
+    }
+
+
+ */
+
+/*
+
+Loops:
+
+ for (int a = 0; a < nums.length; a++) {
+
+       if (nums[a] == 9) {
+                result++;
+            }
+       }return result;
+
+for (Начальное значение переменной; Логическое выражение с переменной(условие выполнения цикла); Действие над переменной,
+ вызываемое при выполнении условия) {
+    Операторы, которые будут выполнять цикл при условии, что логическое выражение - true;
+}
+
+
+Или:
+
+int result = 0;
+int a = 0;
+
+while (a < nums.length) {
+
+if (nums[a] == 9) {
+result++;
+}
+a++;
+}
+return result;
+
+или:
+
+do {
+    Тело цикла;
+} while (условие выполения); он является оператором постусловия (сначала выполнит, потом проверит).
+
+//
+
+Пробежаться по масиву можно еще так:
+
+int [] arr = {1, 2, 4};
+
+for (int elem: arr) {
+action>>>
+
+
+}
+
+while (condition) {
+
+action
+}
+
+
+ */
+
+/*
+
+
+
+/*
+
+Тренарный оператор:
+
+varName = <condition> ? <expression  1> : <expression  2>;
+
+num1 = num1 > 0 ? (num1 + 1): 0ж
+ */
+    }
+
+    // int element: array для каждого элемента масива
+
+
+}
