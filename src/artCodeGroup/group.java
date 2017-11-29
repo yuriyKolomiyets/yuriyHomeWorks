68d688ce0f3659ec664cd0e3e28f47319884194f
yuriykolomiyets <juriy121@gmail.com>
2017-11-29 17:32:37 +0200

    poplava

---
+++ src/artCodeGroup/group.java
@@ -0,0 +1,26 @@
+package artCodeGroup;
+
+import car2.Fine;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class group {
+
+   Student student1 = new student(0, "Yuriy", "Kolomiyets", "ab@aasd.com", 1988, 1000, 1);
+   Student student2 = new student(1, "Olga", "Kolomiyets", "ac@aasd.com", 1988, 1000, 2);
+   Scanner scanner = new Scanner(System.in);
+
+   Student student3 = new student();
+   student3.setUserId(scanner.nextInt());
+   student3.setName(scanner.next());
+
+    // Student student3 = new student(scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt());  а хороша попытка была
+
+
+   // описываю импорт данных со сканера нового студента
+
+   Student[] students = new Student[2];
+   Student[] newStudent = Arrays.copyOf(students, sudent.length+1);
+   Student[] students = newStudent; // кажись тут начехлил с переключением ссылки
+}
