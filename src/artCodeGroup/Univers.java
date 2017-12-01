52f9c63d738f484bf2a5404000a2b65ee3208a7d
yuriykolomiyets <juriy121@gmail.com>
2017-12-01 23:17:18 +0200

    add print method

--- src/artCodeGroup/Univers.java
+++ src/artCodeGroup/Univers.java
@@ -28,6 +28,11 @@ public class Univers {
         String nameFirdstStudent = scanner.next();
         System.out.print("Enter user surname:");
         String surnameFirdstStudent = scanner.next();
+        System.out.print("Enter user email:");
+        String emailFirdstStudent = scanner.next();
+        System.out.print("Enter money:");
+        String moneyFirdstStudent = scanner.next();
+        double moneyFirst = Double.parseDouble(moneyFirdstStudent);
 
         System.out.println("Second student");
         System.out.print("Enter user id:");
@@ -36,28 +41,38 @@ public class Univers {
         String nameSecondStudent = scanner.next();
         System.out.print("Enter user surname:");
         String surnameSecondStudent = scanner.next();
+        System.out.print("Enter user email:");
+        String emailSeondStudent = scanner.next();
+        System.out.print("Enter money:");
+        String moneySecondStudent = scanner.next();
+        double moneySecond = Double.parseDouble(moneySecondStudent);
 
         System.out.println("Third student");
-
         System.out.print("Enter user id:");
         int idThirdStudent = scanner.nextInt();
         System.out.print("Enter user name:");
         String nameThirdStudent = scanner.next();
         System.out.print("Enter user surname:");
         String surnameThirdStudent = scanner.next();
+        System.out.print("Enter user email:");
+        String emailThirdStudent = scanner.next();
+        System.out.print("Enter money:");
+        String moneyThirdStudent = scanner.next();
+        double moneyThird = Double.parseDouble(moneyThirdStudent);
 
 
         Group group1 = new Group(new Student[]{
+                new Student(idFirstStudent, nameFirdstStudent, surnameFirdstStudent, emailFirdstStudent, 1989, 0, 0),
+                new Student(idSecondStudent, nameSecondStudent, surnameSecondStudent, emailSeondStudent, 1989, 0, 0),
+                new Student(idThirdStudent, nameThirdStudent, surnameThirdStudent, emailThirdStudent, 1989, 0, 0),
                 new Student(4, "Ivan", "Iakovlev", "asdf@sdfg.ru", 1989, 0, 0),
                 new Student(5, "Ivan", "Iakovlev", "asdf@sdfg.ru", 1989, 0, 0),
-                new Student(idFirstStudent, nameFirdstStudent, surnameFirdstStudent, "asdf@sdfg.ru", 1989, 0, 0),
-                new Student(idSecondStudent, nameSecondStudent, surnameSecondStudent, "asdf@sdfg.ru", 1989, 0, 0),
-                new Student(idThirdStudent, nameThirdStudent, surnameThirdStudent, "asdf@sdfg.ru", 1989, 0, 0),
-
         });
 
 
-        System.out.println("Total students in the group:" + group1.getCounterNumsofStudent());
+        System.out.println("Total students in the group: " + group1.getCounterNumsofStudent());
+
+        System.out.println("let's print all of them:");
         group1.printAllStudent();
 
 
