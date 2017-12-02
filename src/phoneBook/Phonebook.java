a034d970944fd7215724c4f8d606e54d68851fac
yuriykolomiyets <juriy121@gmail.com>
2017-12-02 10:40:27 +0200

    poplava

--- src/phoneBook/Phonebook.java
+++ src/phoneBook/Phonebook.java
@@ -1,28 +1,28 @@
 package phoneBook;
 
 public class Phonebook {
-    private Phonebook[] friends;
 
     private int countOfFriends;
+    private Friend[] friends;
 
-    public Phonebook(Phonebook[] friends) {
+    public Phonebook(int countOfFriends, Friend[] friends) {
+        this.countOfFriends = countOfFriends;
         this.friends = friends;
-        countOfFriends = friends.length;
     }
 
     public int getCountOfFriends() {
         return countOfFriends;
     }
 
+    public Friend[] getFriends() {
+        return friends;
+    }
+
     public void setCountOfFriends(int countOfFriends) {
         this.countOfFriends = countOfFriends;
     }
 
-    public Phonebook[] getFriends() {
-        return friends;
-    }
-
-    public void setFriends(Phonebook[] friends) {
+    public void setFriends(Friend[] friends) {
         this.friends = friends;
     }
 }
--- src/phoneBook/Smartphone.java
+++ src/phoneBook/Smartphone.java
@@ -5,11 +5,10 @@ public class Smartphone {
 
     public static void main(String[] args){
 
-
-        Smartphone smartpphone1 = new Smartphone(new Phonebook[]{
+        Phonebook phonebook = new Phonebook(new Friend[]{
                 new Friend(1, "Slava", "0509876543", 1988, 83),
                 new Friend(2, "Olga", "9847483", 1988, 55),
-                new Friend(3,"Fisa", "2345", 1988, 55)
+                new Friend(3, "Fisa", "2345", 1988, 55)
         });
 }
 }
