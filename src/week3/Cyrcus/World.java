8cb91b06ebf0846ba8145dd1d41cf74777427614
yuriykolomiyets <juriy121@gmail.com>
2017-12-17 20:51:20 +0200

    cyrcus

--- src/week3/Cyrcus/World.java
+++ src/week3/Cyrcus/World.java
@@ -11,16 +11,16 @@ public class World {
 
         cyrcusArtists.setAllArtists(new ArrayList<Artist>(10));
 
-        Artist tw1 = new TightropeWalker("Ivan1", 1000,0,0);
-        Artist tw2 = new TightropeWalker("Ivan2", 2000,0,0);
-        Artist tw3 = new TightropeWalker("Ivan3", 3000,0,0);
+        Artist tw1 = new TightropeWalker("Ivan1", 1000, 0, 0);
+        Artist tw2 = new TightropeWalker("Ivan2", 2000, 0, 0);
+        Artist tw3 = new TightropeWalker("Ivan3", 3000, 0, 0);
 
-        Artist clown1 = new Clown ("Serhii1", 1500,0,0);
-        Artist clown2 = new Clown("Serhii2", 2500,0,0);
+        Artist clown1 = new Clown("Serhii1", 1500, 0, 0);
+        Artist clown2 = new Clown("Serhii2", 2500, 0, 0);
 
-        Artist acrobat1 = new Acrobat("Iurii1", 1550,0,0);
-        Artist acrobat2 = new Acrobat("Iurii2", 2550,0,0);
-        Artist acrobat3 = new Acrobat("Iurii3", 3550,0,0);
+        Artist acrobat1 = new Acrobat("Iurii1", 1550, 0, 0);
+        Artist acrobat2 = new Acrobat("Iurii2", 2550, 0, 0);
+        Artist acrobat3 = new Acrobat("Iurii3", 3550, 0, 0);
 
         cyrcusArtists.addArtist(tw1);
         cyrcusArtists.addArtist(tw2);
@@ -33,14 +33,13 @@ public class World {
         cyrcusArtists.addArtist(acrobat2);
         cyrcusArtists.addArtist(acrobat3);
 
-
         //1) посмотреть список артистов.
 
         cyrcusArtists.printAllArtists();
 
         //2) нанять на работу нового циркача.
 
-        Artist clown3 = new Clown("Serhii3", 3500,0,0);
+        Artist clown3 = new Clown("Serhii3", 3500, 0, 0);
         cyrcusArtists.addArtist(clown3);
 
         // 3) уволить самого пьющего акробата.
@@ -48,15 +47,14 @@ public class World {
         acrobat1.drink(2);
         acrobat2.drink(1);
 
-       cyrcusArtists.sortByCountOfdrinks();
+        cyrcusArtists.sortByCountOfdrinks();
 
-        for (Artist artist: cyrcusArtists.getAllArtists()) {
-            if (artist instanceof Acrobat){
+        for (Artist artist : cyrcusArtists.getAllArtists()) {
+            if (artist instanceof Acrobat) {
                 cyrcusArtists.fireArtist(artist);
                 break;
             }
         }
-
 
         //4) Отправить кого-то выступать (каждый выступает по разному)
 
