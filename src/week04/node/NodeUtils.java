63f0f5da6c34a06eeffd87ed0613547ecd3492b7
yuriykolomiyets <juriy121@gmail.com>
2018-01-04 09:42:50 +0200

    home tasks in process

--- src/week04/node/NodeUtils.java
+++ src/week04/node/NodeUtils.java
@@ -5,14 +5,12 @@ public class NodeUtils {
         return newNode.next = chainHead;
     }
 
-
     public static void addToTail(Node newNode, Node chainHead) {
         if (chainHead.next == null) {
             chainHead.next = newNode;
             return;
         }
         addToTail(newNode, chainHead.next);
-
     }
 
     public static String toString(Node chain) {
@@ -27,18 +25,37 @@ public class NodeUtils {
 
     public static Node[] toArray(Node chain) {
 
-        return
+        if (chain == null) {
+            return new Node[]{};
 
+        } else {
+            Node[] ret = toArray(chain.next);
+            Node[] res = new Node[ret.length + 1];
+            res[0] = chain;
+            System.arraycopy(ret, 0, res, 1, ret.length);
+            return res;
+        }
     }
 
     public static Node insertByIndex(int index, Node newNode, Node chain) {
 
         if (index <= 0) {
+            newNode.next = chain;
+            return newNode;
 
-            return chain;
+        } else {
+            int currentIndex = 0;
+            if (currentIndex != index) {
+                currentIndex++;
+                insertByIndex(index, newNode, chain.next);
+            }
+            newNode.next = chain.next;
+            chain.next = newNode;
         }
+        return chain;
+    }
 
-        public static Node remove ( int index, Node chain){
+    public static Node remove(int index, Node chain) {
             return null;
         }
 
@@ -48,7 +65,14 @@ public class NodeUtils {
                 return -1;
             }
 
-        }
+            int currentIndex = 0;
+
+            if (chain.value.equals(target)) {
+                return currentIndex;
+            } else {
+                currentIndex++;
+                indexOf(target, chain.next);
+            }
 
         // 1,2,3,4,5 - 5,4,3,2,1
         // create new
